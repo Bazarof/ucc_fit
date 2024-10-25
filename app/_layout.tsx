76079627e9a486ea773886b8333928a35f23b9cc
Juan Jose Baza <baza_1546@outlook.com>
2024-10-24 22:43:20 -0600

    refactor: app/_layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout() {
 
   return (
     <AnimatedAppLoader>
-      <Stack screenOptions={{}}>
+      <Stack>
         <Stack.Screen name='index' options={{headerShown: false, }} />
         <Stack.Screen name='(studentDrawer)' options={{ headerShown: false, }} />
       </Stack>
