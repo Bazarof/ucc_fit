ca9695a34d3d69c3c21cfdce8a2356a1398bc19e
Diego Melo <diego_melo@outlook.es>
2024-11-30 19:21:18 -0600

    feat: Allowing admin to impersonate roles

--- app/adminDrawer/configuracion.tsx
+++ app/adminDrawer/configuracion.tsx
@@ -1,13 +1,30 @@
 import ButtonOption from "@/components/ButtonOption";
 import { View, Text, StyleSheet, ScrollView } from "react-native";
 import { useSession } from "@/components/session/SessionProvider";
+import { useRouter } from "expo-router";
 
 export default function AdminConfig() {
   const { signOut } = useSession();
+  const router = useRouter();
 
   return (
     <ScrollView style={styles.container_scroll}>
       <View style={styles.scroll_item_style}>
+
+        <ButtonOption
+          button_style={styles.button_style}
+          text_style={styles.text_style}
+          title="Ver como estudiante"
+          action={() => router.push("/studentDrawer/(tabs)")}
+        />
+
+        <ButtonOption
+          button_style={styles.button_style}
+          text_style={styles.text_style}
+          title="Ver como entrenador"
+          action={() => router.push("/trainerDrawer/")}
+        />
+
         <ButtonOption
           button_style={styles.button_style}
           text_style={styles.text_style}
@@ -29,6 +46,7 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     borderRadius: 10,
     elevation: 3,
+    marginVertical: 5
   },
   scroll_item_style: {
     paddingTop: 20,
