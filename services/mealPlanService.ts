f940504c188e50ecbce61173eb9fb6b51baeefd9
Diego Melo <diego_melo@outlook.es>
2024-12-08 12:42:58 -0600

    feat: Nutritionist user list

--- services/mealPlanService.ts
+++ services/mealPlanService.ts
@@ -1,10 +1,13 @@
 import MealPlan from "@/types/MealPlan";
 import { getFirestore } from "@react-native-firebase/firestore";
 
-export const getMealPlan = async (userId: string): Promise<MealPlan> => {
+export const getMealPlan = async (userId: string): Promise<MealPlan | null> => {
     const db = getFirestore();
-    const mealPlan = await db.collection("meal_plans").doc(userId.toString()).get();
-    return mealPlan.data() as MealPlan;
+    const mealPlan = await db.collection("meal_plans").doc(userId).get();
+
+    if (!mealPlan.exists) return null;
+
+    return { ...mealPlan.data(), uid: mealPlan.id } as MealPlan;
 }
 
 export const createMealPlan = async (mealPlan: MealPlan) => {
